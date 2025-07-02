b1b39c45dbb0467c1754ba42082ae9ccc8f4bbef
nhl1991 <laslark1991@gmail.com>
2025-07-02 17:00:13 +0900

    modified firebase config path.

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -11,13 +11,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDFZ_1A_G4R6IjolqGwB39R2ub-7Q9sFU0",
-  authDomain: "photo-album-by-next-js.firebaseapp.com",
-  projectId: "photo-album-by-next-js",
-  storageBucket: "photo-album-by-next-js.firebasestorage.app",
-  messagingSenderId: "812743555913",
-  appId: "1:812743555913:web:803e3dfb8da067b426f898",
-  measurementId: "G-NWTN50XG64"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
