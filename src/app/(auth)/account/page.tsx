17928c7637866cebb8661f3426fe4f5d6ee0e3f8
Laslark <laslark1991@gmail.com>
2026-02-11 15:26:03 +0900

    refactor: wrap content with semantic <main> element

--- src/app/(auth)/account/page.tsx
+++ src/app/(auth)/account/page.tsx
@@ -1,10 +1,11 @@
 import AccountContainer from "./_components/AccountContainer";
 
-export default function Page(){
-
-  return(
+export default function Page() {
+  return (
     <>
-      <AccountContainer />
+      <main className="flex gap-y-4 items-center justify-center py-8 my-10 px-4">
+        <AccountContainer />
+      </main>
     </>
-  )
+  );
 }
