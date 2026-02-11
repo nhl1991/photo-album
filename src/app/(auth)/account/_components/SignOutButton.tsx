2da10b2778b08f0d853928ce27ec07dc21753392
Laslark <laslark1991@gmail.com>
2026-02-11 16:07:59 +0900

    Fix: Wrap fetch function with try-catch and set user state to null when sign out. Correct the fetch method to POST.

--- src/app/(auth)/account/_components/SignOutButton.tsx
+++ src/app/(auth)/account/_components/SignOutButton.tsx
@@ -1,16 +1,26 @@
+import { useAuth } from "@/hooks/useAuth";
 import { Logout } from "@/utils/firebase-utils";
 import { useRouter } from "next/navigation";
 
 export default function SignOut() {
   const router = useRouter();
+  const { setUser } = useAuth();
   const onClickLogout = async () => {
-    const response = await fetch("/api/auth/logout", {
-      method: "GET",
-    });
-    if (response.ok) {
-      await Logout();
-      router.push("/signin");
-    } else return alert("Failed to Sign Out. Try again.");
+    try {
+      const response = await fetch("/api/auth/logout", {
+        method: "POST",
+      });
+      if (response.ok) {
+        await Logout();
+        setUser(null);
+        router.push("/signin");
+      } else {
+        alert("Failed to Sign Out. Try again.");
+      }
+    } catch (e) {
+      console.log(e);
+      alert("Failed to Sign Out. Try again.");
+    }
   };
 
   return (
