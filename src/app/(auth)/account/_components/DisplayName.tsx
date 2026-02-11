22c37fab321b0abe9477ea2a5c3d7a9118dc4b74
Laslark <laslark1991@gmail.com>
2026-02-11 15:26:46 +0900

    refactor: Add label tag for button.

--- src/app/(auth)/account/_components/DisplayName.tsx
+++ src/app/(auth)/account/_components/DisplayName.tsx
@@ -6,13 +6,15 @@ import { ChangeEvent, useEffect, useState } from "react";
 
 export default function DisplayName() {
   const [isEdit, setIsEdit] = useState(false); // boolean for Edit Display name.
-  const [username, setUsername] = useState<string>('Anonymous');
+  const [username, setUsername] = useState<string>("Anonymous");
   const { user } = useAuth();
-  useEffect(()=>{
-    if(user && user.displayName) setUsername(user.displayName);
-  },[user])
-  const onClick = async () => {
-    if(username==="") return;
+
+  useEffect(() => {
+    if (user && user.displayName) setUsername(user.displayName);
+  }, [user]);
+
+  const handleOnClick = async () => {
+    if (username === "") return;
     if (!user) return;
     //Switch to Edit.
     if (isEdit === false) setIsEdit(true);
@@ -28,33 +30,30 @@ export default function DisplayName() {
   };
 
   return (
-    <div
-      className={`flex border-2 rounded-2xl px-1 `}
-    >
-      {isEdit ? (
-        <input
-          id="nickname"
-          className="w-72 text-2xl px-4 py-1 text-center outline-0 focus:bg-transparent active:bg-transparent"
-          type="text"
-          placeholder={user?.displayName ? user.displayName : "Anonymous"}
-          onChange={onChangeUsername}
-        />
-      ) : (
-        <label
-          htmlFor="nickname"
-          className="w-max text-2xl px-2 py-1 text-center"
-        >
-          {user?.displayName ? user.displayName : "Anonymous"}
-        </label>
-      )}
-
-      <button className="relative w-8 m-1" onClick={onClick}>
+    <div className="p-2">
+      <label className="px-2 font-bold" htmlFor="nickname">DisplayName</label>
+      <div className={`flex border-2 border-gray-300 rounded-2xl px-1 `}>
         {isEdit ? (
-          <CompleteIcon className="w-6" />
+          <input
+            className="w-72 text-2xl px-4 py-1 text-center outline-0 focus:bg-transparent active:bg-transparent"
+            type="text"
+            placeholder={user?.displayName ? user.displayName : "Anonymous"}
+            onChange={onChangeUsername}
+          />
         ) : (
-          <EditIcon className="w-6" />
+          <p className="w-72 text-2xl px-2 py-1 text-center">
+            {user?.displayName ? user.displayName : "Anonymous"}
+          </p>
         )}
-      </button>
+
+        <button id="nickname" className="relative w-8 m-1" onClick={handleOnClick}>
+          {isEdit ? (
+            <CompleteIcon className="w-6 hover:stroke-cyan-400 cursor-pointer" />
+          ) : (
+            <EditIcon className="w-6 hover:stroke-cyan-400 cursor-pointer" />
+          )}
+        </button>
+      </div>
     </div>
   );
 }
